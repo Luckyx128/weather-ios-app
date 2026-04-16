c1d6444e0b408d6eb58171509502f0774ea869df
Lucas Amorim <lucas.amorim@ambiensys.com.br>
2026-04-16 13:40:43 -0300

    Aula2

--- Wather App/pages/Home.swift
+++ Wather App/pages/Home.swift
@@ -3,19 +3,40 @@ import UIKit
 
 class MinhaViewController: UIViewController {
     
+    private lazy var customView: UIView = {
+        
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     //Quando a tela carrega
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.black
         
-        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        customView.backgroundColor = .green
-        view.addSubview(customView)
+        setupView()
         
     }
     
     private func setupView(){
-        
+        view.backgroundColor =  .red
+
+        setHierarchy()
+        setContraits()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(customView)
+    }
+    
+    private func setContraits() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+        ])
     }
 }
 #endif
